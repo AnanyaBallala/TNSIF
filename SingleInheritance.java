5914bcad524b336c03f1f1ed796a2dd4633a76c7
AnanyaBallala <ananyaballala@gmail.com>
2025-05-08 12:35:07 +0530

    Add files via upload

---
+++ SingleInheritance.java
@@ -0,0 +1,15 @@
+package com.TNSIF.SingleInheritance;
+
+public class SingleInheritance {
+	void Work(){
+		System.out.println("Employee works");
+	}
+	
+
+}
+class Developer extends SingleInheritance{
+	void WriteCode(){
+		System.out.println("Employee writes code");
+	}
+	
+}
---
+++ SingleInheritanceMain.java
@@ -0,0 +1,10 @@
+package com.TNSIF.SingleInheritance;
+
+public class SingleInheritanceMain {
+	public static void main(String [] args){
+		Developer obj= new Developer();
+		obj.WriteCode();
+		obj.Work();
+	}
+
+}
