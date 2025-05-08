f2b6762d87d495c8c82b0aca3dda921c5dc6e8bb
AnanyaBallala <ananyaballala@gmail.com>
2025-05-08 12:39:24 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
-package com.TNSIF.Hierarchial;
+package com.TNSIF.TypesOfVar.Main;
+import com.TNSIF.TypesOfVariables.TypeOfVar;
+
 
 public class Main {
-public static void main(String [] args){
-	Childtwo obj=new Childtwo();
-	obj.two();
-	obj.display();
-	
-}
+	public static void main(String[] args){
+		
+		TypeOfVar obj=new TypeOfVar();
+		obj.numone=40;
+		int res=obj.numone;
+		System.out.println("instance var:"+res);
+		obj.display();
+		System.out.println("Static: "+TypeOfVar.numtwo);
+	}
+
 }
